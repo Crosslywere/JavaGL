974145bb107f1b4d19dceeddc61893f11df0b4d8
Crosslywere <ogboruj@outlook.com>
2025-02-14 06:16:58 +0100

    Updated HelloCube geometry shader - Added an explanation on the normal generated - Optimized the code by moving the inverse calculation outside the loop

--- src/main/resources/shader/cube.geom.glsl
+++ src/main/resources/shader/cube.geom.glsl
@@ -1,4 +1,6 @@
 // Geometry shader that calculates the normal
+// The caveat of using a geometry shader is that
+// the normal is influenced by the view position
 #version 330 core
 layout (triangles) in;
 layout (triangle_strip, max_vertices = 3) out;
@@ -8,10 +10,11 @@ out vec3 fragPos;
 void main() {
 	vec3 T = vec3(gl_in[0].gl_Position) - vec3(gl_in[1].gl_Position); // Tangent
 	vec3 B = vec3(gl_in[0].gl_Position) - vec3(gl_in[2].gl_Position); // BiNormal
+	mat4 imodel = transpose(inverse(model));
 	for (int i = 0; i < 3; i++) {
 		gl_Position = gl_in[i].gl_Position;
-		normal = vec3(transpose(inverse(model)) * vec4(normalize(cross(B, T)), 1.0));
-        fragPos = vec3(transpose(inverse(model)) * gl_Position);
+		normal = vec3(imodel * vec4(normalize(cross(B, T)), 1.0));
+        fragPos = vec3(imodel * gl_Position);
 		EmitVertex();
 	}
 }
